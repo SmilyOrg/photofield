c1ce19835bb0ae763459b9cb6b3a0c42d5c22caa
Miha Lunar <mlunar@gmail.com>
2022-07-08 21:44:45 +0200

    Update dependencies

--- ui/src/components/NaturalViewer.vue
+++ ui/src/components/NaturalViewer.vue
@@ -3,18 +3,6 @@
 
     <page-title :title="pageTitle"></page-title>
     
-    <!-- <div>
-      <div v-if="collectionTask.isRunning">Loading...</div>
-      <div v-else-if="collectionTask.isError">
-        <p>{{ collectionTask.last.error.message }}</p>
-        <button @click="collectionTask.perform">Try again</button>
-      </div>
-      <h2>Collection</h2><pre>{{ collection }}</pre>
-      <h2>Scene Params</h2><pre>{{ sceneParams }}</pre>
-      <h2>Scene</h2><pre>{{ scene }}</pre>
-      <h2>Region</h2><pre>{{ region }}</pre>
-    </div> -->
-
     <tile-viewer
       class="viewer"
       ref="viewer"
--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,9 +1,15 @@
 import vue from '@vitejs/plugin-vue'
+import { visualizer } from "rollup-plugin-visualizer"
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), visualizer()],
   resolve: {
     dedupe: ["vue"],
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
   },
 })
